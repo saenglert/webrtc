ad5c92a711e2ef6de2d003f6f4a9f7fe7a997279
Sascha Englert <sascha@englerts.de>
2018-06-11 15:28:02 +0200

    Testing webrtc capabilties

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
-const connection: RTCPeerConnection = new RTCPeerConnection({});
-const channel = connection.createDataChannel();
+namespace webrtc {
+    let connect: HTMLInputElement;
+    let send: HTMLInputElement;
+    let close: HTMLInputElement;
+
+    const enableConnect = () => (connect.disabled = false);
+    const disableConnect = () => (connect.disabled = true);
+    const enableSend = () => (send.disabled = false);
+    const disableSend = () => (send.disabled = true);
+    const enableClose = () => (close.disabled = false);
+    const disableClose = () => (close.disabled = true);
+
+    const createConnection = () => {};
+
+    const init = () => {
+        connect = document.querySelector('input#connect') as HTMLInputElement;
+        send = document.querySelector('input#send') as HTMLInputElement;
+        close = document.querySelector('input#close') as HTMLInputElement;
+
+        connect.addEventListener('click', createConnection);
+
+        enableConnect();
+    };
+
+    window.addEventListener('load', init);
+}
